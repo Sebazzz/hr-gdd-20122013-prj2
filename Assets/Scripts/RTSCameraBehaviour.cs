b6f2cd99829e2eef5380f1493031a2591c94a5f8
t.kamermans <t.kamermans@gmail.com>
2012-11-13 18:04:10 +0000

    "Space to turn" added to camera

--- Assets/Scripts/RTSCameraBehaviour.cs
+++ Assets/Scripts/RTSCameraBehaviour.cs
@@ -6,6 +6,10 @@ public class RTSCameraBehaviour : MonoBehaviour {
     public float panSpeed = 20;
 	public float zoomSpeed = 25;
 	public float cameraAngle = 45;
+	private enum CameraDirection { NorthEast, NorthWest, SouthEast, SouthWest}
+	private CameraDirection cameraDirection = CameraDirection.NorthEast;
+	
+	private float cameraRotationY = 225;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +19,7 @@ public class RTSCameraBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		HandleCameraMove ();
-		
+        
 		HandleCharacterSelection ();
 		
 		HandleCameraZoom ();
@@ -23,7 +27,7 @@ public class RTSCameraBehaviour : MonoBehaviour {
 
 	void HandleCameraZoom ()
 	{
-		float scroll = Input.GetAxis("ZoomWheel") * zoomSpeed * Time.deltaTime;
+		float scroll = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * Time.deltaTime;
 		
 		if (camera != null) {
 			if (camera.isOrthoGraphic) {
@@ -39,6 +43,8 @@ public class RTSCameraBehaviour : MonoBehaviour {
 		if(Input.GetMouseButtonDown(1)){
 			ShootRay();
 		}
+		
+		
 	}
 
 	void HandleCameraMove ()
@@ -46,6 +52,11 @@ public class RTSCameraBehaviour : MonoBehaviour {
 		Vector3 vector = new Vector3(Input.GetAxis("Horizontal") * panSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * panSpeed * Time.deltaTime);
 		vector = Quaternion.AngleAxis(cameraAngle, Vector3.up) * vector;
 		cameraTransform.Translate(vector, Space.World);
+		
+		if (Input.GetKeyUp("space")) {
+			TurnCameraClockwise(); 
+			cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, Quaternion.Euler(new Vector3(35f, cameraRotationY, 0f)), 1f);
+		}
 	}
 	
 	void ShootRay(){
@@ -66,5 +77,30 @@ public class RTSCameraBehaviour : MonoBehaviour {
 			}
 		}
 
+	}
+
+	private void TurnCameraClockwise() {
+		switch(cameraDirection){
+			case CameraDirection.NorthEast:
+				cameraTransform.Translate(new Vector3(0f, 0f, -30f), Space.World);
+				cameraRotationY = 315f;
+				cameraDirection = CameraDirection.SouthEast;
+				break;
+			case CameraDirection.NorthWest:
+				cameraTransform.Translate(new Vector3(30f, 0f, 0f), Space.World);
+				cameraRotationY = 225f;
+				cameraDirection = CameraDirection.NorthEast;
+				break;
+			case CameraDirection.SouthEast:
+				cameraTransform.Translate(new Vector3(-30f, 0f, 0f), Space.World);
+				cameraRotationY = 45f;
+				cameraDirection = CameraDirection.SouthWest;
+				break;
+			case CameraDirection.SouthWest:
+				cameraTransform.Translate(new Vector3(0f, 0f, 30f), Space.World);
+				cameraRotationY = 135f;
+				cameraDirection = CameraDirection.NorthWest;
+				break;
+		}
 	}
 }
