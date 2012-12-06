990a56c9f5dd15f29e09b7ce2db4f6be5e72de5e
robin.kruyt@gmail.com <robin.kruyt@gmail.com>
2012-12-06 22:20:01 +0000

    Lijnen en shit.

--- src/Assets/Scripts/ControlHerderBehaviour.cs
+++ src/Assets/Scripts/ControlHerderBehaviour.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 
 public class ControlHerderBehaviour : MonoBehaviour {
 	public float waypointSpacing = 5f;
-	
+    
+    private int layerMask = 1 << 8; // Layer 8 is de layer waar hij mee raycast.
+    private float FAILSAFE = 50f; // Failsafe die er voor zorgt dat je geen rare paden krijgt. Stelt de Maximale afstand voor.
+
 	public GameObject ShepherdPath;
 	
     private bool listening = false;
@@ -44,13 +47,20 @@ public class ControlHerderBehaviour : MonoBehaviour {
                 lastAdded = position;
             }
 
-            if (Vector3.Distance(position, lastAdded) > waypointSpacing) {
+            if (Vector3.Distance(position, lastAdded) > waypointSpacing && Vector3.Distance(position, lastAdded) < FAILSAFE) {
                 trajectory.Enqueue(position);
             }
        
         }
 
 	}
+
+    void OnGUI() {
+        /*Vector2 pointA = Camera.main.WorldToViewportPoint(this.transform.position);
+        Vector2 pointB = Camera.main.WorldToViewportPoint(Vector3.zero);
+
+        Drawing.DrawLine(pointA, pointB);*/
+    }
 
 	private void drawPath(){
 		Vector3[] t = trajectory.ToArray();
@@ -70,7 +80,7 @@ public class ControlHerderBehaviour : MonoBehaviour {
         Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
         RaycastHit hit;
 
-        if (Physics.Raycast (ray, out hit)) {
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)) {
 
             return hit.point;
 
