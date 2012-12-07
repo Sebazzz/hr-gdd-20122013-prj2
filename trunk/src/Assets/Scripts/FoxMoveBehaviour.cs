224020ff7c4f8da91cf9fa63db386e295bb44d3d
triadsebas@gmail.com <triadsebas@gmail.com@27ce721a-f892-eb88-cb5f-af1e2911f001>
2012-12-07 14:43:38 +0000

    Moved around some stuff, reworked foxes

--- trunk/src/Assets/Scripts/FoxMoveBehaviour.cs
+++ trunk/src/Assets/Scripts/FoxMoveBehaviour.cs
@@ -1,15 +1,10 @@
 using UnityEngine;
-using System.Collections;
 
 /// <summary>
 /// This script makes sure the 
 /// </summary>
 public class FoxMoveBehaviour : MonoBehaviour {
     private MoveBehaviour moveScript;
-
-    public int DeltaX = 5;
-    public int DeltaY = 5;
-    public int DeltaZ = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -22,13 +17,6 @@ public class FoxMoveBehaviour : MonoBehaviour {
 	        return;
 	    }
 
-        // find an position
-	    Vector3 newPos = this.transform.position;
-        newPos.x += DeltaX;
-        newPos.y += DeltaY;
-        newPos.z += DeltaZ;
-	    
-        // instruct the movement script
-        moveScript.MoveTo(newPos);
+        moveScript.MoveInCurrentDirection();
 	}
 }
--- trunk/src/Assets/Scripts/MoveBehaviour.cs
+++ trunk/src/Assets/Scripts/MoveBehaviour.cs
@@ -1,41 +1,117 @@
 using UnityEngine;
-using System.Collections;
 
+/// <summary>
+/// Enables movement of the object
+/// </summary>
 public class MoveBehaviour : MonoBehaviour {
     /// <summary>
     /// Target of the movement. Only the <see cref="Vector3.x"/> and <see cref="Vector3.z"/> components will be used.
     /// </summary>
-    public Vector3 Target;
+    private Vector3 target;
 
+    /// <summary>
+    /// Takes the target as direction
+    /// </summary>
+    private bool targetAsDirection;
+
+    /// <summary>
+    /// The number of units the object will move per time
+    /// </summary>
     public float Speed = 5;
+
+    /// <summary>
+    /// Defines the number of units to 
+    /// </summary>
+    public float TargetRadius = 2;
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    float distance = Mathf.Abs(Vector3.Distance(this.transform.position, Target));
-
-        if (distance > 1) {
-	        // move somewhat
-	        float xdiff = CalculateSpeed(this.Target.x, this.transform.position.x, this.Speed) * Time.deltaTime;
-	        float zdiff = CalculateSpeed(this.Target.z, this.transform.position.z, this.Speed) * Time.deltaTime;
-
-	        this.transform.Translate(new Vector3(xdiff, 0, zdiff));
-	    } else {
-	        this.enabled = false;
-	    }
+	   if (targetAsDirection) {
+	       this.MoveTowardsDirection();
+	   } else {
+	       this.MoveTowardsPoint();
+	   }
 	}
+
+    /// <summary>
+    /// Executes movement logic to move to a specific point as set in the <see cref="MoveTo"/> method (<see cref="target"/>)
+    /// </summary>
+    private void MoveTowardsPoint() {
+        Vector3 currentPosition = this.gameObject.transform.position;
+
+        // check if we reached the target
+        if (Vector3.Distance(currentPosition, target) < TargetRadius) {
+            // we're done
+            this.Stop();
+            return;
+        }
+
+        // calculate the movement 
+        Vector3 difference = target - currentPosition;
+        Vector3 currentMovement = difference * Time.deltaTime * Speed;
+
+        // check if we will not cross over the target
+        Vector3 targetAfterMovement = currentPosition + currentMovement;
+
+        this.transform.position = targetAfterMovement;
+    }
+
+    /// <summary>
+    /// Executes movement logic to move to a specific direction as set in the <see cref="MoveToDirection"/> method (<see cref="target"/>)
+    /// </summary>
+    private void MoveTowardsDirection() {
+        // calculate movement
+        this.gameObject.transform.Translate(0, 0, this.Speed * Time.deltaTime);
+    }
 
     /// <summary>
     /// Enables the script for movement towards the specified target. Only the <see cref="Vector3.x"/> and <see cref="Vector3.z"/> components will be used.
     /// </summary>
-    /// <param name="target"></param>
-    public void MoveTo (Vector3 target) {
+    /// <param name="targetPoint"></param>
+    public void MoveTo (Vector3 targetPoint) {
         this.enabled = true;
-        this.Target = target;
+
+        this.target = targetPoint;
+        this.targetAsDirection = false;
+
+        this.transform.LookAt(targetPoint);
+    }
+
+    /// <summary>
+    /// Enables the script for movement in the direction of the current game object
+    /// </summary>
+    /// <param name="speed"></param>
+    public void MoveInCurrentDirection (float speed) {
+        this.Speed = speed;
+
+        this.MoveToDirection(this.transform.rotation);
+    }
+
+    /// <summary>
+    /// Enables the script for movement in the direction of the specified target.
+    /// </summary>
+    /// <param name="directionTarget"></param>
+    public void MoveToDirection(Quaternion directionTarget) {
+        MoveToDirection(directionTarget, this.Speed);
+    }
+
+    /// <summary>
+    /// Enables the script for movement in the direction of the specified target.
+    /// </summary>
+    /// <param name="directionTarget"></param>
+    /// <param name="speed"></param>
+    public void MoveToDirection(Quaternion directionTarget, float speed) {
+        this.enabled = true;
+
+        this.targetAsDirection = true;
+        this.Speed = speed;
+
+        this.transform.rotation = directionTarget;
     }
 
     /// <summary>
@@ -45,13 +121,10 @@ public class MoveBehaviour : MonoBehaviour {
         this.enabled = false;
     }
 
-    private float CalculateSpeed(float target, float current, float speed) {
-        float calcSpeed = Mathf.Abs(target - current) < speed ? Mathf.Abs(target - current) : speed;
-
-        if (target < current) {
-            calcSpeed *= -1f;
-        }
-
-        return calcSpeed;
+    /// <summary>
+    /// Executes movement in the rotation of the current object, moves in the current speed
+    /// </summary>
+    public void MoveInCurrentDirection() {
+        this.MoveInCurrentDirection(this.Speed);
     }
 }
