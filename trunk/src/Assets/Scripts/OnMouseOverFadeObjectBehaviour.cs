7ac2482cfd18418c332d2291fac60008a5c7a2a9
t.kamermans <t.kamermans@27ce721a-f892-eb88-cb5f-af1e2911f001>
2012-12-10 14:27:24 +0000

    OnMouseOverFadeObjectBehaviour.cs added

---
+++ trunk/src/Assets/Scripts/OnMouseOverFadeObjectBehaviour.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+
+public class OnMouseOverFadeObjectBehaviour : MonoBehaviour {
+
+	private Renderer renderer;
+	private float alpha = 1f;
+	public float alphaMin = 0.3f;
+	public float alphaMax = 1f;
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	private void Start () {
+	
+	}
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update () {
+
+		Debug.Log(alpha);
+
+		renderer = gameObject.collider.renderer;
+
+		if ((renderer.material.color.a >= alpha && alpha == alphaMax) ||
+			(renderer.material.color.a <= alpha && alpha == alphaMin)) return;
+
+		if (renderer) {
+			Color shadercolor = new Color(
+				renderer.material.color.r,
+				renderer.material.color.g,
+				renderer.material.color.b,
+				Mathf.Lerp(renderer.material.color.a, alpha, Time.deltaTime*5f));
+			renderer.material.shader = Shader.Find("Transparent/Diffuse");
+			renderer.material.color = shadercolor;
+		}
+	}
+
+	/// <summary>
+	/// On Mouse Over Collider of the game object
+	/// </summary>
+	void OnMouseOver() {
+		alpha = alphaMin;
+	}
+
+	void OnMouseExit() {
+		alpha = alphaMax;
+	}
+}
