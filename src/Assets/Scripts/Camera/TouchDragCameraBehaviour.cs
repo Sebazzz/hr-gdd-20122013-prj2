c28df3abb402d25ea28de3341444020fc7e8e27d
t.kamermans <t.kamermans@gmail.com>
2012-12-10 10:32:58 +0000

    updated camerabehaviour

--- src/Assets/Scripts/Camera/TouchDragCameraBehaviour.cs
+++ src/Assets/Scripts/Camera/TouchDragCameraBehaviour.cs
@@ -3,18 +3,27 @@ using System.Collections;
 
 public class TouchDragCameraBehaviour : MonoBehaviour {
 
+	//The target which the camera is set to look at from the start of the script.
+	//This property changes the cameraLookAtTarget, which is private for safety reasons
 	public Vector3 cameraLookAtTargetStart = new Vector3();
 	private Vector3 cameraLookAtTarget = Vector3.zero;
+	//The distance of the camera to the CameraLookAtTarget
 	public Vector3 CameraToTargetDistance = new Vector3(0f, 20f, -7.5f);
 
 	private Vector3 OnMouseDownCursorPoint = new Vector3();
 	private Vector3 OnMouseDownCameraPosition = new Vector3();
-	private Vector3 MouseMoveDistance = new Vector3();
 
 	/// <summary>
 	/// Use this for initialization
 	/// </summary>
 	private void Start () {
+		//Set CameraLookAtTargetStart the default if the cameralookattarget is set as vector3.zero
+		if(cameraLookAtTargetStart == Vector3.zero)
+			cameraLookAtTargetStart = new Vector3(
+				Camera.main.transform.position.x - CameraToTargetDistance.x,
+				Camera.main.transform.position.y - CameraToTargetDistance.y,
+				Camera.main.transform.position.z - CameraToTargetDistance.z);
+
 		cameraLookAtTarget = cameraLookAtTargetStart;
 
 		transform.position = new Vector3(
@@ -59,9 +68,6 @@ public class TouchDragCameraBehaviour : MonoBehaviour {
 	/// Used to calculate the movement of the camera
 	/// </summary>
 	private void OnMouse() {
-
-
-		
 		transform.position = new Vector3(
 			OnMouseDownCameraPosition.x + ((Input.mousePosition.x - OnMouseDownCursorPoint.x) * -0.1f),
 			OnMouseDownCameraPosition.y,
@@ -69,7 +75,7 @@ public class TouchDragCameraBehaviour : MonoBehaviour {
 	}
 
 	/// <summary>
-	/// 
+	/// Reset OnMouseDownCameraPosition, so it can't effect the following touchdown
 	/// </summary>
 	private void OnMouseUp() {
 		OnMouseDownCameraPosition = Vector3.zero;
