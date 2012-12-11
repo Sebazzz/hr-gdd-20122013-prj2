99f8f6ec7cea2316514b7faec5d8da1886dc7cfa
triadsebas@gmail.com <triadsebas@gmail.com@27ce721a-f892-eb88-cb5f-af1e2911f001>
2012-12-11 17:55:31 +0000

    Modify touch size

--- trunk/src/Assets/Scripts/ControlHerderBehaviour.cs
+++ trunk/src/Assets/Scripts/ControlHerderBehaviour.cs
@@ -11,7 +11,7 @@ public class ControlHerderBehaviour : MonoBehaviour {
     public Material normal_material;
     public Material fast_material;
 
-    private float selectRadius = 1f; // Sphere radius for the dog selector.
+    private float selectRadius = 2f; // Sphere radius for the dog selector.
     private float range = 150f; // Sphere casting is heavy, so keep the range low
     
     private int layerMask = 1 << 8; // Layer 8 is de layer waar hij mee raycast.
