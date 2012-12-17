16abcaf738316089b61e449639db010a7e327cbf
t.kamermans <t.kamermans@27ce721a-f892-eb88-cb5f-af1e2911f001>
2012-12-17 12:27:16 +0000

    camera edited

--- trunk/src/Assets/Scripts/Camera/TouchDragCameraBehaviour.cs
+++ trunk/src/Assets/Scripts/Camera/TouchDragCameraBehaviour.cs
@@ -3,50 +3,44 @@ using System.Collections;
 
 public class TouchDragCameraBehaviour : MonoBehaviour {
 
-	//The target which the camera is set to look at from the start of the script.
-	//This property changes the cameraLookAtTarget, which is private for safety reasons
-	public Vector3 cameraLookAtTargetStart = new Vector3();
-	private Vector3 cameraLookAtTarget = Vector3.zero;
-	//The distance of the camera to the CameraLookAtTarget
-	public Vector3 CameraToTargetDistance = new Vector3(0f, 20f, -7.5f);
+	////The target which the camera is set to look at from the start of the script.
+	////This property changes the cameraLookAtTarget, which is private for safety reasons
+	//public Vector3 cameraLookAtTargetStart = new Vector3();
+	//private Vector3 cameraLookAtTarget = Vector3.zero;
+	////The distance of the camera to the CameraLookAtTarget
+	//public Vector3 CameraToTargetDistance = new Vector3(0f, 20f, -7.5f);
 
 	private Vector3 OnMouseDownCursorPoint = new Vector3();
 	private Vector3 OnMouseDownCameraPosition = new Vector3();
+
+	public Vector2 BoundingBoxBottomLeft;
+	public Vector2 BoundingBoxTopRight;
 
 	/// <summary>
 	/// Use this for initialization
 	/// </summary>
 	private void Start () {
-		//Set CameraLookAtTargetStart the default if the cameralookattarget is set as vector3.zero
-		if(cameraLookAtTargetStart == Vector3.zero)
-			cameraLookAtTargetStart = new Vector3(
-				Camera.main.transform.position.x - CameraToTargetDistance.x,
-				Camera.main.transform.position.y - CameraToTargetDistance.y,
-				Camera.main.transform.position.z - CameraToTargetDistance.z);
+		////Set CameraLookAtTargetStart the default if the cameralookattarget is set as vector3.zero
+		//if(cameraLookAtTargetStart == Vector3.zero)
+		//    cameraLookAtTargetStart = new Vector3(
+		//        Camera.main.transform.position.x - CameraToTargetDistance.x,
+		//        Camera.main.transform.position.y - CameraToTargetDistance.y,
+		//        Camera.main.transform.position.z - CameraToTargetDistance.z);
 
-		cameraLookAtTarget = cameraLookAtTargetStart;
+		//cameraLookAtTarget = cameraLookAtTargetStart;
 
-		transform.position = new Vector3(
-				cameraLookAtTarget.x + this.CameraToTargetDistance.x,
-				cameraLookAtTarget.y + this.CameraToTargetDistance.y,
-				cameraLookAtTarget.z + this.CameraToTargetDistance.z);
+		//transform.position = new Vector3(
+		//        cameraLookAtTarget.x + this.CameraToTargetDistance.x,
+		//        cameraLookAtTarget.y + this.CameraToTargetDistance.y,
+		//        cameraLookAtTarget.z + this.CameraToTargetDistance.z);
 
-		transform.LookAt(cameraLookAtTarget);
+		//transform.LookAt(cameraLookAtTarget);
 	}
 	
 	/// <summary>
 	/// Update is called once per frame
 	/// </summary>
 	private void Update () {
-		//if (Input.GetMouseButtonDown(0)) {
-		//    RaycastHit hit = new RaycastHit();
-		//    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		//    if (Physics.Raycast(ray, out hit)) {
-		//        foreach (GameObject go in GameObject.FindGameObjectsWithTag(Tags.Shepherd)) {
-		//            if (hit.collider.gameObject == go.gameObject) return;
-		//        }
-		//    }
-		//}
 
 		if (Input.GetMouseButtonDown(0) && !MouseManager.TryAcquireLock(this)) {
 			return;
@@ -55,6 +49,17 @@ public class TouchDragCameraBehaviour : MonoBehaviour {
 		if (Input.GetMouseButtonDown(0)) OnMouseDown();
 		if (Input.GetMouseButton(0) && OnMouseDownCameraPosition != Vector3.zero) OnMouse();
 		if (Input.GetMouseButtonUp(0)) OnMouseUp();
+
+		float x = transform.position.x;
+		float z = transform.position.z;
+
+		if (transform.position.x < BoundingBoxBottomLeft.x) x = BoundingBoxBottomLeft.x;
+		if (transform.position.x > BoundingBoxTopRight.x) x = BoundingBoxTopRight.x;
+		if (transform.position.z < BoundingBoxBottomLeft.y) z = BoundingBoxBottomLeft.y;
+		if (transform.position.z > BoundingBoxTopRight.y) z = BoundingBoxTopRight.y;
+
+		transform.position = new Vector3(x, transform.position.y, z);
+
 	}
 
 	/// <summary>
@@ -85,12 +90,5 @@ public class TouchDragCameraBehaviour : MonoBehaviour {
 		
 		if(MouseManager.CurrentLockOwner == this)
 			MouseManager.ReleaseLock(this);
-	}
-
-	/// <summary>
-	/// Returns the cameraLookAtTarget Vector 3, so other objects can have the center point of the camera
-	/// </summary>
-	public Vector3 GetCameraLookAtTarget() {
-		return cameraLookAtTarget;
 	}
 }
