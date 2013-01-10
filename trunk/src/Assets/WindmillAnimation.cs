a0c4516b89c395f99e601e57453cc880b7287d39
triadsebas@gmail.com <triadsebas@gmail.com@27ce721a-f892-eb88-cb5f-af1e2911f001>
2013-01-10 16:36:29 +0000

    Donut magnetism

---
+++ trunk/src/Assets/WindmillAnimation.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class WindmillAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Animation>().Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
