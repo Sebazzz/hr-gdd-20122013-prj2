bbbd4e8692a917fa33d20fe4cb316062e7a6b306
Robin.Kruyt@gmail.com <robin.kruyt@gmail.com>
2013-01-11 09:47:34 +0000

    CURSOR BITCHES

---
+++ src/Assets/Scripts/Input/CursorController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class CursorController : MonoBehaviour {
+    public Texture cursorImage;
+
+	// Use this for initialization
+	void Start () {
+	    Screen.showCursor = false;
+
+        if (cursorImage == null) {
+            throw new Exception("A cursor image is needed.");
+        }
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+	    Vector3 mousePos = Input.mousePosition;
+        Rect position = new Rect(mousePos.x, Screen.height - mousePos.y, cursorImage.width, cursorImage.height);
+        GUI.Label(position, cursorImage);
+	}
+}
