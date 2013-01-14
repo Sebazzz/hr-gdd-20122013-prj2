8b51d058c6f1ea4cfe4e15ccd49e0bca63119a35
t.kamermans <t.kamermans@gmail.com>
2013-01-14 13:35:09 +0000

    Options

---
+++ src/Assets/Scripts/MainMenuOptionsSaveBehaviour.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainMenuOptionsSaveBehaviour : MonoBehaviour {
+
+	public MainMenuOptionsBehaviour optionsBehaviour;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUpAsButton() {
+		optionsBehaviour.SaveOptions();
+	}
+}
