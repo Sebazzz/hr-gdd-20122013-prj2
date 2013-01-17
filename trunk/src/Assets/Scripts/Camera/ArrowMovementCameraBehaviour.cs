90cf984a6d86440cd953c4c7790f2bfc601aaf7a
t.kamermans <t.kamermans@27ce721a-f892-eb88-cb5f-af1e2911f001>
2013-01-17 14:45:33 +0000

    camera rotation edited

--- trunk/src/Assets/Scripts/Camera/ArrowMovementCameraBehaviour.cs
+++ trunk/src/Assets/Scripts/Camera/ArrowMovementCameraBehaviour.cs
@@ -176,12 +176,27 @@ public class ArrowMovementCameraBehaviour : MonoBehaviour {
 	/// Used to calculate the movement of the camera
 	/// </summary>
 	private void OnMouse() {
-	    float x = this.onMouseDownCameraPosition.x + ((Input.mousePosition.x - this.onMouseDownCursorPoint.x)*-0.1f);
-	    float y = this.onMouseDownCameraPosition.z + ((Input.mousePosition.y - this.onMouseDownCursorPoint.y)*-0.1f);
+		//float x = this.onMouseDownCameraPosition.x + ((Input.mousePosition.x - this.onMouseDownCursorPoint.x)*-0.1f);
+		//float y = this.onMouseDownCameraPosition.z + ((Input.mousePosition.y - this.onMouseDownCursorPoint.y)*-0.1f);
 
-	    Vector3 newPos = new Vector3(x, this.onMouseDownCameraPosition.y, y);
+		//Vector3 newPos = new Vector3(x, this.onMouseDownCameraPosition.y, y);
 
-	    transform.position = newPos;
+		//transform.position = newPos;
+		
+		//transform.position = new Vector3(
+		//    this.onMouseDownCameraPosition.x + ((Input.mousePosition.x - this.onMouseDownCursorPoint.x) * -0.1f),
+		//    this.onMouseDownCameraPosition.y,
+		//    this.onMouseDownCameraPosition.z + ((Input.mousePosition.y - this.onMouseDownCursorPoint.y) * -0.1f));
+
+		
+		Vector3 v = new Vector3(
+			this.onMouseDownCameraPosition.x + ((Input.mousePosition.x - this.onMouseDownCursorPoint.x) * -0.1f),
+			this.onMouseDownCameraPosition.y,
+			this.onMouseDownCameraPosition.z + ((Input.mousePosition.y - this.onMouseDownCursorPoint.y) * -0.1f));
+
+		v = Quaternion.AngleAxis(transform.rotation.y, Vector3.up) * v;
+		transform.position = v;
+
 	}
 
 	/// <summary>
