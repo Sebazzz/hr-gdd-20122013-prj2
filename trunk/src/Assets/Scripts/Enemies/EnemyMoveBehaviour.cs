f392184f7b2a4493d7dde8ee1c015c4cae2680c3
t.kamermans <t.kamermans@27ce721a-f892-eb88-cb5f-af1e2911f001>
2013-01-20 21:18:23 +0000

    http://24.media.tumblr.com/tumblr_m9ob36lP7J1rbosrro1_1280.jpg

---
+++ trunk/src/Assets/Scripts/Enemies/EnemyMoveBehaviour.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyMoveBehaviour : MonoBehaviour {
+
+	public AudioSource audio;
+	public AudioClip clip;
+	private bool active = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!active) return;
+		transform.position = Vector3.Lerp(transform.position, new Vector3(180f, transform.position.y, transform.position.z), 0.005f);
+
+	}
+
+	public void Activate() {
+		active = true;
+		audio.Stop();
+		audio.clip = clip;
+		audio.Play();
+		audio.loop = true;
+		
+	}
+}
