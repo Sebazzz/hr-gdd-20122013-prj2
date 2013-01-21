aaa83f975c863f3b79e7ff6dc439fedc26ab80bb
triadsebas@gmail.com <triadsebas@gmail.com>
2013-01-21 14:13:55 +0000

    Prototype loading screen

--- src/Assets/Scripts/Support/Scenes.cs
+++ src/Assets/Scripts/Support/Scenes.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-/// <summary>
+﻿/// <summary>
 /// Contains references to scenes, hardcoded
 /// </summary>
 public class Scenes {
-
     public const string Playground = "Playground";
 
     public const string MainMenu = "MainMenu";
-}
 
+    public const string LoadingScreen = "LoadingScreen";
+}
