fe120e1ba96a4e491fb45681e7ecbb0fbcf1209f
t.kamermans <t.kamermans@gmail.com>
2013-01-24 08:52:20 +0000

    Main Menu scripts moved to MainMenu map

---
+++ src/Assets/Scripts/MainMenu/MainMenuReturnToLevelSelectBehaviour.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+public static class MainMenuReturnToLevelSelectBehaviour{
+
+	public enum SelectedWorld { None, Scotland, Holland, Canada, Australia}
+	public static SelectedWorld selectedWorld = SelectedWorld.None;
+}
