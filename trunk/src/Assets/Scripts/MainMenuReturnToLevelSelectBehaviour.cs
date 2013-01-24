13f7fd954e2da7b97c5d90debd97ba63f3c63d46
t.kamermans <t.kamermans@27ce721a-f892-eb88-cb5f-af1e2911f001>
2013-01-24 08:59:19 +0000

    derp

--- trunk/src/Assets/Scripts/MainMenuReturnToLevelSelectBehaviour.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public static class MainMenuReturnToLevelSelectBehaviour{
-
-	public enum SelectedWorld { None, Scotland, Holland, Canada, Australia}
-	public static SelectedWorld selectedWorld = SelectedWorld.None;
-}
