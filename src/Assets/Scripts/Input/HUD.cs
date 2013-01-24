d8faf39e79ba629d9b787d0289643fcd8d42fba4
Robin.Kruyt@gmail.com <robin.kruyt@gmail.com>
2013-01-24 14:02:46 +0000

    GUI

--- src/Assets/Scripts/Input/HUD.cs
+++ src/Assets/Scripts/Input/HUD.cs
@@ -7,20 +7,40 @@ public class HUD : MonoBehaviour {
     // Skin data
     public GUISkin skin;
 
-    private bool showDialog = false; // Is set to true once a dialog is shown
+    public MainMenuReturnToLevelSelectBehaviour.SelectedWorld currentWorld;
+
+    enum DialogType{
+        none = 0,
+        text = 1,
+        score = 2,
+        death = 3
+    }
+    private DialogType showDialog = DialogType.none;
     private Rect dialogRect;
 
     //Dialog data
     private string dialog_title = "";
     private string dialog_text = "";
-    private string dialog_yes = "Yes";
-    private string dialog_no = "No";
+    private string dialog_yes = "";
+    private string dialog_no = "";
     private Action action_yes;
     private Action action_no;
+
+    // Score data
+    private Boolean minScore;
+    private Boolean maxScore;
+    private Boolean maxScoreTime;
 
     private Texture2D timeTexture;
     private Texture2D goalTexture;
     private Texture2D sheepTexture;
+
+    private Texture2D timeScoreTexture;
+    private Texture2D timeScoreCheckedTexture;
+    private Texture2D minScoreTexture;
+    private Texture2D minScoreCheckedTexture;
+    private Texture2D maxScoreTexture;
+    private Texture2D maxScoreCheckedTexture;
 
     private int goal = 0;
     private int collected = 0;
@@ -39,14 +59,21 @@ public class HUD : MonoBehaviour {
             throw(new Exception("GUISkin is needed for the HUD"));
         }
 
-        loadTextures();
 
+        loadTextures();
 	}
 
     private void loadTextures() {
         timeTexture = Resources.Load("Hud/hud-time") as Texture2D;
         goalTexture = Resources.Load("Hud/hud-goal") as Texture2D;
         sheepTexture = Resources.Load("Hud/hud-sheep") as Texture2D;
+
+        timeScoreTexture = Resources.Load("Hud/time") as Texture2D;
+        timeScoreCheckedTexture = Resources.Load("Hud/time-checked") as Texture2D;
+        minScoreTexture = Resources.Load("Hud/min_sheep") as Texture2D;
+        minScoreCheckedTexture = Resources.Load("Hud/min_sheep-checked") as Texture2D;
+        maxScoreTexture = Resources.Load("Hud/max_sheep") as Texture2D;
+        maxScoreCheckedTexture = Resources.Load("Hud/max_sheep-checked") as Texture2D;
     }
 	
 	// Update is called once per frame
@@ -59,8 +86,12 @@ public class HUD : MonoBehaviour {
     void OnGUI() {
         drawTopBar();
 
-        if (showDialog) {
+        if (showDialog == DialogType.text) {
             drawDialog();
+        } else if (showDialog == DialogType.score) {
+            drawScoreDialog();
+        } else if (showDialog == DialogType.death) {
+            drawDeathDialog();
         }
         
     }
@@ -97,30 +128,116 @@ public class HUD : MonoBehaviour {
     /// <param name="yes_action">Action delegate to run for Yes</param>
     /// <param name="no_action">Action delegate to run for No</param>
     public void DisplayDialog(string title, string text, Action yes_action, Action no_action) {
-        dialog_title = title;
+        dialog_title = "";//title;
         dialog_text = text;
         action_yes = yes_action;
         action_no = no_action;
-        showDialog = true;
+        showDialog = DialogType.text;
     }
 
     private void drawDialog() {
-        dialogRect = GUI.Window(0, dialogRect, drawInsideDialog, dialog_title);
+        dialogRect = GUI.Window(0, dialogRect, drawInsideDialog, dialog_title, skin.window);
     }
 
     private void drawInsideDialog(int dialogID) {
-        GUI.Label(new Rect(25, 25, 250, 130), dialog_text);
+        GUI.Label(new Rect(25, 25, 250, 130), dialog_text, skin.GetStyle("WindowText"));
 
-        if (GUI.Button(new Rect(30, 160, 50, 30), dialog_yes)) {
+        if (GUI.Button(new Rect(30, 160, 50, 30), dialog_yes, skin.GetStyle("YesButton"))) {
             action_yes();
-            showDialog = false;
+            showDialog = DialogType.none;
         }
 
-        if (GUI.Button(new Rect(220, 160, 50, 30), dialog_no)) {
+        if (GUI.Button(new Rect(220, 160, 50, 30), dialog_no, skin.GetStyle("NoButton"))) {
             action_no();
-            showDialog = false;
+            showDialog = DialogType.none;
         }
     }
+
+    /// <summary>
+    /// Displays A score dialog.
+    /// </summary>
+    public void DisplayScoreDialog(Boolean minScore, Boolean maxScore, Boolean maxScoreTime) {
+        dialog_text = "Level done";
+        this.minScore = minScore;
+        this.maxScore = maxScore;
+        this.maxScoreTime = maxScoreTime;
+        showDialog = DialogType.score;
+    }
+
+    private void drawScoreDialog() {
+        dialogRect = GUI.Window(0, dialogRect, drawInsideScoreDialog, dialog_title, skin.window);
+    }
+
+    private void drawInsideScoreDialog(int dialogID) {
+        GUI.Label(new Rect(25, 25, 250, 130), dialog_text, skin.GetStyle("WindowText"));
+
+        Texture2D minT = minScoreTexture;
+        if(minScore){
+            minT = minScoreCheckedTexture;
+        }
+
+        Texture2D maxT = maxScoreTexture;
+        if (maxScore) {
+            maxT = maxScoreCheckedTexture;
+        }
+
+        Texture2D maxTimeT = timeScoreTexture;
+        if (maxScoreTime) {
+            maxTimeT = timeScoreCheckedTexture;
+        }
+
+
+        GUI.DrawTexture(new Rect(150 - 90 - 40 , 60, 80, 80), minT, ScaleMode.StretchToFill, true, 0);
+        GUI.DrawTexture(new Rect(150 - 40, 60, 80, 80), maxT, ScaleMode.StretchToFill, true, 0);
+        GUI.DrawTexture(new Rect(150 + 90 - 40, 60, 80, 80), maxTimeT, ScaleMode.StretchToFill, true, 0);
+
+
+        if (GUI.Button(new Rect(150 - 50 - 20, 150, 40, 40), "", skin.GetStyle("MenuScoreButton"))) {
+            showDialog = DialogType.none;
+            Application.LoadLevel(Scenes.MainMenu);
+        }
+
+        if (GUI.Button(new Rect(150-20, 150, 40, 40), "", skin.GetStyle("RestartScoreButton"))) {
+            showDialog = DialogType.none;
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        if (GUI.Button(new Rect(150 + 50 - 20, 150, 40, 40), "", skin.GetStyle("NextScoreButton"))) {
+            showDialog = DialogType.none;
+            MainMenuReturnToLevelSelectBehaviour.selectedWorld = currentWorld;
+            Application.LoadLevel(Scenes.MainMenu);
+        }
+    }
+
+    /// <summary>
+    /// Displays A death dialog.
+    /// </summary>
+    public void DisplayDeathDialog(String reason) {
+        dialog_text = reason;
+        
+        showDialog = DialogType.death;
+    }
+
+    private void drawDeathDialog() {
+        dialogRect = GUI.Window(0, dialogRect, drawInsideDeathDialog, "", skin.window);
+    }
+
+    private void drawInsideDeathDialog(int dialogID) {
+        GUI.Label(new Rect(25, 25, 250, 130), dialog_text, skin.GetStyle("WindowText"));
+
+
+        if (GUI.Button(new Rect(150 - 50, 150, 40, 40), "", skin.GetStyle("MenuScoreButton"))) {
+            showDialog = DialogType.none;
+            Application.LoadLevel(Scenes.MainMenu);
+        }
+
+        if (GUI.Button(new Rect(150 + 10, 150, 40, 40), "", skin.GetStyle("RestartScoreButton"))) {
+            showDialog = DialogType.none;
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+    
 
     private void drawTopBar() {
         GUI.DrawTexture(new Rect(pixelsFromLeft(20), 20, 115, 59), timeTexture, ScaleMode.StretchToFill, true, 0);
