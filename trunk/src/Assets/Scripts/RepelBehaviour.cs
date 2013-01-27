d58b34e00d32a12e206112ad61ac3656d9a4df8c
triadsebas@gmail.com <triadsebas@gmail.com@27ce721a-f892-eb88-cb5f-af1e2911f001>
2013-01-27 15:06:46 +0000

    Dog effect angle implementation

--- trunk/src/Assets/Scripts/RepelBehaviour.cs
+++ trunk/src/Assets/Scripts/RepelBehaviour.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 
 public class RepelBehaviour : MonoBehaviour {
     private DogAudioController audioController;
-
     private bool hasBarked;
-    public float power = 50.0F;
-    public float radius = 10.0F;
+
+    public float OuterPower = 1500f;
+    public float OuterRadius = 12f;
 
     public float InnerPower = 0;
     public float InnerRadius = 0;
+    
+    /// <summary>
+    /// Specifies if force is only applied when the object is in the angle specified by <see cref="EffectAngle"/>
+    /// </summary>
+    public bool UseEffectAngle = false;
+
+    /// <summary>
+    /// Specifies the angle an object must be in from the center of the rotation of this object. The angle is the total angle in front of the object.
+    /// For 
+    /// </summary>
+    public float EffectAngle = 90;
 
     public bool Perform2DInnerRadiusCheck = true;
 
@@ -18,25 +29,50 @@ public class RepelBehaviour : MonoBehaviour {
     }
 
     private void Update() {
+        // push sheep
         Vector3 explosionPos = this.transform.position;
 
-        Collider[] colliders = Physics.OverlapSphere(explosionPos, this.radius);
-
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, this.OuterRadius);
         bool hasSeenSheep = false;
         foreach (Collider hit in colliders) {
-            float force = this.InnerRadiusCheck(hit.transform) ? this.InnerPower : this.power;
-            float radiusForce = this.InnerRadiusCheck(hit.transform) ? this.InnerRadius : this.radius;
+            // check tag
+            if (hit.tag != Tags.Sheep) {
+                continue;
+            }
+
+            // check for drag
+            if (Math.Abs(hit.rigidbody.drag - 0) <= 0.01) {
+                continue;
+            }
+
+            // check for angle
+            if (this.UseEffectAngle) {
+                Vector3 dir = hit.transform.position - this.transform.position;
+
+                float angle = Vector3.Angle(this.transform.forward, dir);
+
+                Debug.Log(String.Format("Direction: {0}; my Y: {1}; angle: {2}", dir, this.transform.eulerAngles.y,
+                                        angle));
+
+                if (angle > EffectAngle/2f) {
+                    continue;
+                }
+
+            }
+
+            // check for radius
+            float force = this.InnerRadiusCheck(hit.transform) ? this.InnerPower : this.OuterPower;
+            float radiusForce = this.InnerRadiusCheck(hit.transform) ? this.InnerRadius : this.OuterRadius;
 
             if (force < 0.1) {
                 continue;
             }
 
-            if (hit.tag == Tags.Sheep && Math.Abs(hit.rigidbody.drag - 0) > 0.01) {
-                hit.rigidbody.AddExplosionForce(force, explosionPos, radiusForce, 0);
-                hasSeenSheep = true;
-            }
+            hit.rigidbody.AddExplosionForce(force, explosionPos, radiusForce, 0);
+            hasSeenSheep = true;
         }
 
+        // bark
         if (this.audioController != null) {
             if (!this.hasBarked && hasSeenSheep) {
                 this.audioController.SheepProximitySound.Play();
@@ -46,6 +82,23 @@ public class RepelBehaviour : MonoBehaviour {
             if (this.hasBarked && !hasSeenSheep) {
                 // Reset bark
                 this.hasBarked = false;
+            }
+        }
+
+        // debug helpers
+        if (Debug.isDebugBuild && this.UseEffectAngle) {
+            float splitAngle = this.EffectAngle / 2f;
+
+            {
+                Vector3 position = (this.transform.position +
+                                    AngleVector3(this.transform.TransformDirection(Vector3.forward*this.OuterRadius), splitAngle));
+                Debug.DrawLine(this.transform.position, position, Color.red, 0);
+            }
+
+            {
+                Vector3 position = (this.transform.position +
+                                    AngleVector3(this.transform.TransformDirection(Vector3.forward * this.OuterRadius), -splitAngle));
+                Debug.DrawLine(this.transform.position, position, Color.red, 0);
             }
         }
     }
@@ -70,10 +123,37 @@ public class RepelBehaviour : MonoBehaviour {
 
 
     private void OnDrawGizmosSelected() {
+        // draw outer radius
         Gizmos.color = Color.grey;
-        Gizmos.DrawWireSphere(this.transform.position, this.radius);
+        Gizmos.DrawWireSphere(this.transform.position, this.OuterRadius);
 
+        // draw inner radius
         Gizmos.color = Color.black;
         Gizmos.DrawWireSphere(this.transform.position, this.InnerRadius);
+
+        // draw both markers for the angle
+        if (this.UseEffectAngle) {
+            float splitAngle = this.EffectAngle / 2f;
+
+            Gizmos.color = Color.red;
+            {
+                Vector3 position = (this.transform.position +
+                                    AngleVector3(this.transform.TransformDirection(Vector3.forward * this.OuterRadius), splitAngle));
+                Gizmos.DrawLine(this.transform.position, position);
+            }
+
+            {
+                Vector3 position = (this.transform.position +
+                                    AngleVector3(this.transform.TransformDirection(Vector3.forward * this.OuterRadius), -splitAngle));
+                Gizmos.DrawLine(this.transform.position, position);
+            }
+        }
     }
+
+    private static Vector3 AngleVector3(Vector3 point, float angle) {
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+
+        return rotation * point;
+    }
+
 }
