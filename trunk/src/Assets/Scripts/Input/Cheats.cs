57585a92b1d57cc355385a208a8b4fe98f59a507
triadsebas@gmail.com <triadsebas@gmail.com@27ce721a-f892-eb88-cb5f-af1e2911f001>
2013-01-27 22:11:13 +0000

    Sheep Rain  Some stay dry and others feel the pain  Sheep Rain  A baby born will die before the sin
     Sheep Rain
     The school books say it can't be here again
     Sheep Rain
     The prisons make you wonder where it went
    
     Sheep Rain
     Build a tent and say the world as dry
     Sheep Rain
     Zoom the camera out and see the light
    
     Sheep Rain
     Forecast to be falling yesterday
     Sheep Rain
     Only in the past is what they say
    
     Sheep Rain
     Raised your neighborhood insurance rates
     Sheep Rain
     Makes us happy 'livin in a gay
    
     Sheep Rain
     Made me cross the street the other day
     Sheep Rain
     Made you turn your head the other way
    
     (Chorus)
     Sheep Rain
     History quickly crashing through your veins
     Sheep Rain
     Using you to fall back down again
     [Repeat]
    
     Sheep Rain
     Seldom mentioned on the radio
     Sheep Rain
     It's the fear your leaders car control
    
     Sheep Rain
     Worse than swearing, worse than calling names
     Sheep Rain
     Say it publicly, and you're insane
    
     Sheep Rain
     No one wants to hear about it now
     Sheep Rain
     Wish real hard it goes away somehow
    
     Sheep Rain
     Makes the best of friends begin to fight
     Sheep Rain
     But did they know each other in the light?
    
     Sheep Rain
     Every February washed away
     Sheep Rain
     Stays behind as colors celebrate
    
     Sheep Rain
     The same crime has a higher price to pay
     Sheep Rain
     The judge and jury swear it's not in the face
    
     (Chorus)
    
     Sheep Rain
     Dirty secrets of economy
     Sheep Rain
     Turns that body into GDP
    
     Sheep Rain
     The bell curve blames the baby's DNA
     Sheep Rain
     But test scores are how much the parents made
    
     Sheep Rain
     Flippin' cousin France the other night
     Sheep Rain
     Cleans the sewers out beneath Mumbai
    
     Sheep Rain
     'Cross the world and back it's all the same
     Sheep Rain
     Angels cry and shake their heads in shame
    
     Sheep Rain
     Lifts the ark of paradise in sin
     Sheep Rain
     Which part do you think you're 'livin in?
    
     Sheep Rain
     More than marchin' more than past and law
     Sheep Rain
     Remake how we got to where we are
    
     (Chorus)

--- trunk/src/Assets/Scripts/Input/Cheats.cs
+++ trunk/src/Assets/Scripts/Input/Cheats.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
 
 /// <summary>
 /// Class that contains all cheats implementation and provides a basic cheat dialog implementation
@@ -13,17 +16,10 @@ public static class Cheats {
     #region Cheats Implementation
     // ReSharper disable UnusedMember.Local
     /// <summary>
-    /// Cheat implementation class
+    /// Cheat implementation class. These public static members are called via reflection only.
     /// </summary>
     private static class Impl {
-        private static readonly List<CheatVar<bool>> ToggleCheatVars = new List<CheatVar<bool>>();
-        private static readonly List<CheatVar<float>> FloatCheatVars = new List<CheatVar<float>>();
-
-        static Impl() {
-            ToggleCheatVars.Add(new CheatVar<bool>("supersheep", "Enlarge all sheeps in the next levels 4 times", v => CheatsController.EnableLargeSheep = v));
-            ToggleCheatVars.Add(new CheatVar<bool>("gamecheats", "Show the in-game cheat button and menu", v => CheatsController.EnableInGameCheatsMenu = v));
-            ToggleCheatVars.Add(new CheatVar<bool>("terrainbounce", "Enable a bouncy terrain", v => CheatsController.TerrainBounce = v));
-        }
+        #region Functional (useful) cheats
 
         [Cheat("Help")]
         public static void ShowCheatsHelpReference() {
@@ -100,11 +96,6 @@ public static class Cheats {
             GameCheatReferenceDialog.ShowDialog("Cheats Reference", cheatName.ToArray(), cheatDescription.ToArray(), "MonospaceLabel");
         }
 
-        [Cheat("PlayTheGround")]
-        public static void LoadDeveloperSandboxLevel() {
-            AsyncSceneLoader.Load(Scenes.Playground);
-        }
-
         [Cheat("ClearSettings")]
         public static void ClearAllSettings() {
             PlayerPrefs.DeleteAll();
@@ -125,28 +116,9 @@ public static class Cheats {
             AsyncSceneLoader.Load(Scenes.MainMenu);
         }
 
-        [Cheat("EndlessCameraMovement")]
-        public static void DisableCameraMovementBounds() {
-            ArrowMovementCameraBehaviour cameraScript = Camera.mainCamera.GetComponent<ArrowMovementCameraBehaviour>();
+        #endregion
 
-            if (cameraScript != null) {
-                cameraScript.BoundingBoxBottomLeft = new Vector2(-1000, -1000);
-                cameraScript.BoundingBoxTopRight = new Vector2(1000, 1000);
-            }
-        }
-
-        [Cheat("SheepRocket")]
-        public static void LaunchAllSheepUpIntoTheAirByTheSpecifiedForce(float force) {
-            GameObject[] sheep = GameObject.FindGameObjectsWithTag(Tags.Sheep);
-
-            foreach (GameObject gameObject in sheep) {
-                Rigidbody rb = gameObject.rigidbody;
-
-                float finalForce = force*rb.mass;
-
-                rb.AddRelativeForce(Vector3.up * finalForce, ForceMode.Impulse);
-            }
-        }
+        #region Variabele modifications
 
         [Cheat("enable")]
         public static void EnableTheSpecifiedVariabele(string variabele) {
@@ -159,7 +131,7 @@ public static class Cheats {
         }
 
         private static void SetBooleanVariabeleValue(string variabele, bool value) {
-// select member of cheat controller
+            // select member of cheat controller
             foreach (var cheatVar in ToggleCheatVars) {
                 if (String.Equals(cheatVar.Name, variabele, StringComparison.InvariantCultureIgnoreCase)) {
                     cheatVar.Setter.Invoke(value);
@@ -172,6 +144,15 @@ public static class Cheats {
             SimpleTextDialog.ShowDialog("Error", String.Format("Variabele could not be set to '{1}': Variabele '{0}' does not exist.", variabele, value), "MonospaceLabel");
         }
 
+        private static readonly List<CheatVar<bool>> ToggleCheatVars = new List<CheatVar<bool>>();
+        private static readonly List<CheatVar<float>> FloatCheatVars = new List<CheatVar<float>>();
+
+        static Impl() {
+            ToggleCheatVars.Add(new CheatVar<bool>("supersheep", "Enlarge all sheeps in the next levels 4 times", v => CheatsController.EnableLargeSheep = v));
+            ToggleCheatVars.Add(new CheatVar<bool>("gamecheats", "Show the in-game cheat button and menu", v => CheatsController.EnableInGameCheatsMenu = v));
+            ToggleCheatVars.Add(new CheatVar<bool>("terrainbounce", "Enable a bouncy terrain", v => CheatsController.TerrainBounce = v));
+            ToggleCheatVars.Add(new CheatVar<bool>("sheeprotationlock", "Rotation lock of sheep. By default true.", v => CheatsController.EnableSheepRotationLock = v));
+        }
 
         #region Nested type: CheatVar
 
@@ -194,6 +175,138 @@ public static class Cheats {
         }
 
         #endregion
+
+        #endregion
+
+        #region Unlockables
+
+        [Cheat("PlayTheGround")]
+        public static void LoadDeveloperSandboxLevel() {
+            AsyncSceneLoader.Load(Scenes.Playground);
+        }
+
+        #endregion
+
+        #region Testing Helpers
+
+        [Cheat("EndlessCameraMovement")]
+        public static void DisableCameraMovementBounds() {
+            ArrowMovementCameraBehaviour cameraScript = Camera.mainCamera.GetComponent<ArrowMovementCameraBehaviour>();
+
+            if (cameraScript != null) {
+                cameraScript.BoundingBoxBottomLeft = new Vector2(-1000, -1000);
+                cameraScript.BoundingBoxTopRight = new Vector2(1000, 1000);
+            }
+        }
+
+        #endregion
+
+        #region Just for Fun
+        
+        [Cheat("SheepRain")]
+        public static void StartsRainingSheepAllOverTheLevel(int amountPerSecond, int timeInSeconds) {
+            // find a sheep to clone
+            GameObject sheepToClone = GameObject.FindGameObjectWithTag(Tags.Sheep);
+
+            StartCoroutine(SheepRainExecutor(amountPerSecond, timeInSeconds, sheepToClone));
+        }
+
+        private static IEnumerator SheepRainExecutor(int amountPerSecond, int timeInSeconds, GameObject sheepTemplate) {
+            float timeLeft = timeInSeconds;
+
+            // determine terrain bounds
+            Vector3 lowerBounds = Terrain.activeTerrain.transform.position;
+            Vector3 upperBounds = lowerBounds + Terrain.activeTerrain.terrainData.size;
+
+            // split in two
+            int amountPerSecondLower = Mathf.FloorToInt(amountPerSecond / 2f);
+            int amountPerSecondUpper = Mathf.CeilToInt(amountPerSecond / 2f);
+
+            while (timeLeft > 0) {
+                // first half of second
+                for (int n=0;n<amountPerSecondLower;n++) {
+                    SpawnSheep(sheepTemplate, lowerBounds, upperBounds);
+                }
+
+                yield return new WaitForSeconds(0.5f);
+                timeLeft -= 0.5f;
+
+                // second half of second
+                for (int n = 0; n < amountPerSecondUpper; n++) {
+                    SpawnSheep(sheepTemplate, lowerBounds, upperBounds);
+                }
+
+                yield return new WaitForSeconds(0.5f);
+                timeLeft -= 0.5f;
+            }
+
+            yield break;
+        }
+
+        private static void SpawnSheep(GameObject sheepTemplate, Vector3 lowerBounds, Vector3 upperBounds) {
+            // determine position
+            Vector3 spawnPosition = new Vector3(Random.Range(lowerBounds.x, upperBounds.x),
+                                                Camera.mainCamera.transform.position.y + 50f,
+                                                Random.Range(lowerBounds.z, upperBounds.z));
+
+            // determine rotation
+            Vector3 rotation = new Vector3();
+            rotation.y = Random.Range(0, 360);
+
+            if (!CheatsController.EnableSheepRotationLock) {
+                rotation.x = Random.Range(0, 360);
+                rotation.z = Random.Range(0, 360);
+            }
+
+            Object.Instantiate(sheepTemplate, spawnPosition, Quaternion.Euler(rotation));
+        }
+
+        [Cheat("SheepRocket")]
+        public static void LaunchAllSheepUpIntoTheAirByTheSpecifiedForce(float force) {
+            GameObject[] sheep = GameObject.FindGameObjectsWithTag(Tags.Sheep);
+
+            foreach (GameObject gameObject in sheep) {
+                Rigidbody rb = gameObject.rigidbody;
+
+                float finalForce = force*rb.mass;
+
+                rb.AddRelativeForce(Vector3.up * finalForce, ForceMode.Impulse);
+            }
+        }
+
+        #endregion
+
+        #region Support methods
+
+        private static bool StartCoroutine(IEnumerator coroutineResult) {
+            CheatsController controller = AssertGetCheatsController();
+
+            if (controller == null) {
+                return false;
+            }
+
+            controller.StartCoroutine(coroutineResult);
+            return true;
+        }
+
+        private static CheatsController AssertGetCheatsController() {
+            GameObject worldObject = GameObject.FindGameObjectWithTag(Tags.World);
+
+            if (worldObject != null) {
+                CheatsController ctrl = worldObject.GetComponent<CheatsController>();
+
+                if (ctrl != null) {
+                    return ctrl;
+                }
+            }
+
+            SimpleTextDialog.ShowDialog("Error", "You're not currently in a level and the command cannot be executed therefore.", "MonospaceLabel");
+            return null;
+        }
+
+        
+
+        #endregion
     }
     // ReSharper restore UnusedMember.Local
     #endregion
@@ -213,8 +326,6 @@ public static class Cheats {
     }
 
     #endregion
-
-
 
     #region Nested type: Dialog
 
@@ -372,7 +483,7 @@ public static class Cheats {
     }
 
     #endregion
-
+    
     private static class GameCheatReferenceDialog {
         private static Rect _DialogRect;
         private static bool _ShowDialog;
