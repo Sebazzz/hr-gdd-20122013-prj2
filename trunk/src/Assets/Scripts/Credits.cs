207b602d0e6d78a0bd5c4f19008c5352c4832ab4
t.kamermans <t.kamermans@27ce721a-f892-eb88-cb5f-af1e2911f001>
2013-01-29 15:07:46 +0000

    Johnny Derp

---
+++ trunk/src/Assets/Scripts/Credits.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public GameObject credits;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		credits.transform.Translate(Vector3.up * 0.05f);
+
+		if (credits.transform.position.y > 160f) Application.LoadLevelAsync("MainMenu");
+	
+	}
+}
