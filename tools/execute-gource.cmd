2e3bbb933e7b28517f2234f288164a818cb102ef
triadsebas@gmail.com <triadsebas@gmail.com>
2013-01-31 12:45:41 +0000

    Gource space problem resolved

--- tools/execute-gource.cmd
+++ tools/execute-gource.cmd
@@ -5,6 +5,6 @@ echo Changing dir...
 cd ".."
 cd
 svn log -r 1:HEAD --xml --verbose --quiet > subversion-log.me.xml
-%gourceBinDir%\gource.exe -1280x720 --user-image-dir ".\users.me" --default-user-image ".\src\Assets\Textures\Icons\icon_16px.png" --title "Shifting Sheep" --camera-mode overview --file-idle-time 0 --file-filter "(.*)\.meta" subversion-log.me.xml
+"%gourceBinDir%\gource.exe" -1280x720 --user-image-dir ".\users.me" --default-user-image ".\src\Assets\Textures\Icons\icon_16px.png" --title "Shifting Sheep" --camera-mode overview --file-idle-time 0 --file-filter "(.*)\.meta" subversion-log.me.xml
 erase subversion-log.me.xml
 pause
