3e7c7864cd135b08569d1d649c4df0738ea06220
Robin.Kruyt@gmail.com <robin.kruyt@gmail.com>
2013-01-31 12:02:25 +0000

    Turn around terug gezet naar oude versie.

--- src/Assets/Scripts/Enemies/FoeTurnAroundOnCollisionBehaviour.cs
+++ src/Assets/Scripts/Enemies/FoeTurnAroundOnCollisionBehaviour.cs
@@ -30,7 +30,6 @@ public class FoeTurnAroundOnCollisionBehaviour : MonoBehaviour {
             Tags.Enemy,
             Tags.Foe,
             Tags.Sheep,
-            Tags.Shepherd,
             Tags.LevelBounds
         };
 
@@ -53,6 +52,7 @@ public class FoeTurnAroundOnCollisionBehaviour : MonoBehaviour {
         int collidingObjectLayer = collisionInfo.gameObject.layer;
 
         if (!this.ShouldIgnoreObject(collidingObjectTag, collidingObjectLayer)) {
+            //this.Bounce(collisionInfo);
             this.TurnAround();
         }
     }
@@ -73,6 +73,32 @@ public class FoeTurnAroundOnCollisionBehaviour : MonoBehaviour {
         return false;
     }
 
+    // WERKT NIET IN DE CURRENT STATE.
+    private void Bounce(Collision coll) {
+        if ((Time.time - this.lastTurnAroundTime) < this.IgnoreTime) {
+           return;
+        }
+        this.lastTurnAroundTime = Time.time;
+
+        MoveBehaviour mv = this.gameObject.GetComponent<MoveBehaviour>();
+        mv.Stop();
+
+
+        Quaternion rotation = this.gameObject.transform.rotation;
+        ContactPoint hit = coll.contacts[0];
+        Vector3 reflection = Vector3.Reflect(this.gameObject.transform.position.normalized, hit.point.normalized);
+        rotation = Quaternion.Euler(reflection);
+
+        //rotation.x = 0;
+        //rotation.z = 0;
+        mv.MoveToDirection(rotation);
+
+        for (int i = 0; i < this.NumberOfStepBacks; i++) {
+            // execute a single step to prevent being stuck
+            mv.MoveSingleStep();
+        }  
+    }
+
     private void TurnAround() {
         if ((Time.time - this.lastTurnAroundTime) < this.IgnoreTime) {
             return;
@@ -82,12 +108,13 @@ public class FoeTurnAroundOnCollisionBehaviour : MonoBehaviour {
         MoveBehaviour mv = this.gameObject.GetComponent<MoveBehaviour>();
         mv.Stop();
 
+        
         // set the new rotation
         Quaternion rotation = this.gameObject.transform.rotation;
         rotation = Quaternion.Euler(rotation.eulerAngles.x, (rotation.eulerAngles.y + 180) % 360, rotation.eulerAngles.z);
         mv.MoveToDirection(rotation);
 
-        this.transform.Translate(2, 0, 0);
+  
 
         for (int i = 0; i < this.NumberOfStepBacks; i++) {
             // execute a single step to prevent being stuck
