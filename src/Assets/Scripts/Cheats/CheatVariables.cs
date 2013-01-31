c233c609c9156fca1ad79e84f742530a4c72d13e
triadsebas@gmail.com <triadsebas@gmail.com>
2013-01-31 22:16:35 +0000

    Refactored some more

--- src/Assets/Scripts/Cheats/CheatVariables.cs
+++ src/Assets/Scripts/Cheats/CheatVariables.cs
@@ -7,18 +7,18 @@ public static class CheatVariables {
     /// <summary>
     /// Specifies if the rotation lock on the sheep <see cref="Rigidbody"/> is disabled
     /// </summary>
-    [CheatVariabele("supersheep", "Enlarge all sheeps in the next levels 4 times")]
+    [CheatVariabele("supersheep", "Enlarge all sheeps in the next levels 4 times.")]
     public static bool EnableSheepRotationLock = true;
 
     /// <summary>
     /// Specifies if the in-game cheat menu is enabled
     /// </summary>
-    [CheatVariabele("gamecheats", "Show the in-game cheat button and menu")]
+    [CheatVariabele("gamecheats", "Show the in-game cheat button and menu.")]
     public static bool EnableInGameCheatsMenu = false;
 
-    [CheatVariabele("terrainbounce", "Enable a bouncy terrain")]
+    [CheatVariabele("terrainbounce", "Enable a bouncy terrain.")]
     public static bool EnableLargeSheep = false;
 
-    [CheatVariabele("sheeprotationlock", "Rotation lock of sheep. By default true.")]
+    [CheatVariabele("sheeprotationlock", "Lock x and z rotation of sheep. By default true.")]
     public static bool TerrainBounce = false;
 }
